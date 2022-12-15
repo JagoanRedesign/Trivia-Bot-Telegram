0151dc2c8c6f32c2122c23462eb3e2695d234268
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-15 19:19:48 +0700

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,5 @@
+telegram_token = "5751479065:AAG2ao2BIIYkaS4mifds7J7XLD-fPNi1rpg" # Telegram Bot Token
+db_host = "containers-us-west-161.railway.app"        # Database host name
+db_name = "railway"        # Database database name
+db_user = "postgres"        # Database user name
+db_password = "ivvNMwAukusk2O1COC9K"    # Database user password
